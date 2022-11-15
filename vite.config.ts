8a81676139c63c31dcd90141782af79d7c9a3f74
kahosan <15116355657l@gmail.com>
2022-11-15 21:45:06 +0800

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import unocss from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact(), unocss()],
-  publicDir: './',
   envPrefix: 'HOME_',
+  base: './',
   resolve: {
     alias: {
       '@': '/src'
