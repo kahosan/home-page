ec1358115f9486a479322ca007cf59abda36a147
kahosan <15116355657l@gmail.com>
2022-11-16 10:21:19 +0800

    fix: props position

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -7,6 +7,6 @@ interface Props {
 
 export function Link({ href, children, ...props }: Props & JSX.IntrinsicElements['a']) {
   return (
-    <a href={href} className={`${props.className || ''} link`} {...props}>{children}</a>
+    <a {...props} href={href} className={`${props.className || ''} link`}>{children}</a>
   );
 }
