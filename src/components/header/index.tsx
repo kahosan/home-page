51e8df80b46808fe58262916e1653bb7b455812c
kahosan <15116355657l@gmail.com>
2022-11-25 11:46:31 +0800

    feat: add confirm save modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -23,6 +23,7 @@ export default function Header() {
 
   const [openGithubModal, setOpenGithubModal] = useState(false);
   const [openAddModal, setOpenAddModal] = useState(false);
+  const [openConfirmModal, setOpenConfirmModal] = useState(false);
   const [newApp, setNewApp] = useState<ServiceData>({
     name: '',
     path: '',
@@ -37,16 +38,21 @@ export default function Header() {
     setOpenAddModal(false);
   };
 
+  const handleSave = () => {
+    handleSaveService();
+    toggleEditMode();
+  };
+
   return (
     <header className="fixed top-0 right-0 left-0 z-999 blur-bg dark:bg-[rgba(12,12,12,0.65)] bg-[rgb(241,241,241,0.65)]">
       <div className="flex justify-between items-center p-4 max-w-5xl mx-auto h-60px">
         <h3>{import.meta.env.HOME_HEADER_TITLE || 'Data Center'}</h3>
         <div className="flex items-baseline">
           <div onClick={() => handleRestoreService()} className={`${isGithubPages && editMode ? 'visible op-100' : 'invisible op-0'} transition-all i-carbon-reset text-5 mr-3 cursor-pointer opacity-animation-3`} />
-          <div onClick={() => handleSaveService()} className={`${isGithubPages && editMode ? 'visible op-100' : 'invisible op-0'} transition-all i-carbon-save text-5 mr-3 cursor-pointer opacity-animation-3`} />
+          <div onClick={() => setOpenConfirmModal(true)} className={`${isGithubPages && editMode ? 'visible op-100' : 'invisible op-0'} transition-all i-carbon-save text-5 mr-3 cursor-pointer opacity-animation-3`} />
           <div onClick={() => setOpenAddModal(true)} className={`${isGithubPages && editMode ? 'visible op-100' : 'invisible op-0'} transition-all i-carbon-task-add text-5 mr-3 cursor-pointer opacity-animation-3`} />
           <div onClick={() => setOpenGithubModal(true)} className={`${isGithubPages ? 'visible op-100' : 'invisible op-0'} mr-3 transition-all i-carbon-logo-github text-5 mr-3 cursor-pointer opacity-animation-3`} />
-          <div onClick={() => toggleEditMode()} className={`mr-3 opacity-animation-3 ${editMode ? 'i-carbon-edit-off' : 'i-carbon-edit'} text-5 cursor-pointer icon-tap-color`} />
+          <div onClick={() => toggleEditMode()} className={`${isGithubPages ? 'visible op-100' : 'invisible op-0'} ${editMode ? 'i-carbon-edit-off' : 'i-carbon-edit'} text-5 cursor-pointer icon-tap-color mr-3 opacity-animation-3 `} />
           <ThemeToggle />
         </div>
       </div>
@@ -71,6 +77,17 @@ export default function Header() {
             </Modal.Action>
           </div>
         </form>
+      </Modal>
+      <Modal title="确认要保存嘛？" open={openConfirmModal}>
+        <p className="text-center px-5 pb-5">将会使用 Github API 更新 services.json</p>
+        <div className="flex">
+          <Modal.Action type="button" onClick={() => setOpenConfirmModal(false)}>
+            不要
+          </Modal.Action>
+          <Modal.Action type="button" onClick={() => handleSave()}>
+            保存
+          </Modal.Action>
+        </div>
       </Modal>
     </header>
   );
