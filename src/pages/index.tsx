daec845ec1e3ac9f3df7b9a4aeaa7a36c931f0db
kahosan <15116355657l@gmail.com>
2023-02-03 16:03:13 +0800

    feat: add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Header from '../components/header';
 import DataCenter from '../components/data-center';
 
 export default function HomePage() {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <title>BQ Center</title>
+      </Head>
       <Header />
       <DataCenter />
     </>
