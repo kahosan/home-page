fde7097c9159b48f73f186180fa0ca9b4c08b452
kahosan <15116355657l@gmail.com>
2023-02-03 17:33:53 +0800

    test: tmp

--- src/pages/api/services/[method].ts
+++ src/pages/api/services/[method].ts
@@ -4,29 +4,29 @@ import type { Service } from 'src/types/services';
 import { generatorRespError } from 'src/utils/handler';
 import { addServicesData, delServicesData } from 'src/utils/services';
 
-const addHandler: Handler = async (req, res) => {
+const addHandler: Handler = (req, res) => {
   const data = JSON.parse(req.body) as Service;
 
   if (!data) {
     res.status(400).json(generatorRespError('数据不能为空'));
   }
 
-  await addServicesData(data);
+  addServicesData(data);
   res.status(200).json({ msg: `添加 ${data.name} 成功` });
 };
 
-const delHandler: Handler = async (req, res) => {
+const delHandler: Handler = (req, res) => {
   const data = req.body;
 
   if (!data) {
     res.status(400).json(generatorRespError('删除的目标卡片名为空'));
   }
 
-  await delServicesData(data);
+  delServicesData(data);
   res.status(200).json({ msg: `删除 ${data} 成功` });
 };
 
-const handler: Handler = async (req, res) => {
+const handler: Handler = (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).json(generatorRespError(`请求方法 ${req.method} 不支持`));
   } else {
--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,5 +1,4 @@
-import { readFile, writeFile } from 'fs/promises';
-
+import fs from 'fs';
 import { tmpdir } from 'os';
 import path from 'path';
 
@@ -7,20 +6,20 @@ import type { Service } from 'src/types/services';
 
 const filePath = process.env.FILE_PATH ? tmpdir() + process.env.FILE_PATH : `${path.resolve()}/services.json`;
 
-export const getServicesData = async () => {
-  return JSON.parse(await readFile(filePath, { encoding: 'utf-8' })) as Service[];
+export const getServicesData = () => {
+  return JSON.parse(fs.readFileSync(filePath, { encoding: 'utf-8' })) as Service[];
 };
 
-export const addServicesData = async (service: Service) => {
-  const servicesData = await getServicesData();
+export const addServicesData = (service: Service) => {
+  const servicesData = getServicesData();
   servicesData.push(service);
 
-  await writeFile(filePath, JSON.stringify(servicesData, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(servicesData, null, 2));
 };
 
-export const delServicesData = async (target: string) => {
-  const servicesData = await getServicesData();
+export const delServicesData = (target: string) => {
+  const servicesData = getServicesData();
   const newData = servicesData.filter(service => service.name !== target);
 
-  await writeFile(filePath, JSON.stringify(newData, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(newData, null, 2));
 };
