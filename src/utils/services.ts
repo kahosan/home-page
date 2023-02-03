fb360b32176de494f66932ac2c3d7b5b30e40436
kahosan <15116355657l@gmail.com>
2023-02-03 16:36:25 +0800

    test: build with serverless

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,24 +1,24 @@
 import { readFile, writeFile } from 'fs/promises';
-import { resolve } from 'path';
+import path from 'path';
 
 import type { Service } from 'src/types/services';
 
-const ServiceFilePath = `${resolve()}/services.json`;
+const filePath = process.env.FILE_PATH || `${path.resolve()}/services.json`;
 
 export const getServicesData = async () => {
-  return JSON.parse(await readFile(ServiceFilePath, { encoding: 'utf-8' })) as Service[];
+  return JSON.parse(await readFile(filePath, { encoding: 'utf-8' })) as Service[];
 };
 
 export const addServicesData = async (service: Service) => {
   const servicesData = await getServicesData();
   servicesData.push(service);
 
-  await writeFile(ServiceFilePath, JSON.stringify(servicesData, null, 2));
+  await writeFile(filePath, JSON.stringify(servicesData, null, 2));
 };
 
 export const delServicesData = async (target: string) => {
   const servicesData = await getServicesData();
   const newData = servicesData.filter(service => service.name !== target);
 
-  await writeFile(ServiceFilePath, JSON.stringify(newData, null, 2));
+  await writeFile(filePath, JSON.stringify(newData, null, 2));
 };
