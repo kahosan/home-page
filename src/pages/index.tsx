a535d15cf11be4218ffdee97d8172f8f675f02aa
kahosan <15116355657l@gmail.com>
2023-02-05 16:43:06 +0800

    feat: get default data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
 import Head from 'next/head';
+
+import { SWRConfig } from 'swr';
+
 import Header from '../components/header';
 import DataCenter from '../components/data-center';
 
-export default function HomePage() {
+import type { Service } from 'src/types/services';
+
+export default function HomePage({ fallback }: { fallback: Service[] }) {
   return (
-    <>
+    <SWRConfig value={{ fallback }}>
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <title>BQ Center</title>
       </Head>
       <Header />
       <DataCenter />
-    </>
+    </SWRConfig>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.API_URL}/api/services`);
+  const servicesData = await res.json();
+
+  return {
+    props: {
+      fallback: {
+        '/api/services': servicesData
+      }
+    }
+  };
 }
