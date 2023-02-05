c9b478ed4cc669faf87b46959be1397b96ab7916
kahosan <15116355657l@gmail.com>
2023-02-05 17:09:57 +0800

    chore: update edit method

--- src/types/next-handler.d.ts
+++ src/types/next-handler.d.ts
@@ -2,4 +2,4 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
 
-export type EditMethod = 'add' | 'del'
+export type EditMethod = 'add' | 'del' | 'edit'
