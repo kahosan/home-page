2595f0d3485fa7e892ccf7d517218133841b8ba3
kahosan <15116355657l@gmail.com>
2023-05-21 23:43:59 +0800

    feat: use `forgetti` improve app

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withForgetti = require('forgetti-loader/next');
+
+const nextConfig = withForgetti({
+  // Forgetti options. See https://github.com/lxsmnsyc/forgetti/tree/main#configuration for more details.
+  preset: 'react',
+})({
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   distDir: 'dist',
   experimental: {
   },
-};
+});
 
 module.exports = nextConfig;
