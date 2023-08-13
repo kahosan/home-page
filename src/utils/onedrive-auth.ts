6e4fd63bd035dbe4eb35f89104ef82ed6ce5e23c
kahosan <15116355657l@gmail.com>
2023-08-13 19:56:12 +0800

    fix: duplicate parameter added

--- src/utils/onedrive-auth.ts
+++ src/utils/onedrive-auth.ts
@@ -4,19 +4,19 @@ export const authApi = 'https://login.microsoftonline.com/common/oauth2/v2.0';
 export const scope = 'Files.ReadWrite offline_access';
 export const redirectUri = 'http://localhost';
 
-const params = new URLSearchParams({
-  client_id: CLIENT_ID,
-  redirect_uri: redirectUri,
-  client_secret: CLIENT_SECRET
-});
-
 export const getAuthCode = () => {
   window.open(`${authApi}/authorize?client_id=${CLIENT_ID}&scope=${encodeURIComponent(scope)}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}`, '_blank');
 };
 
+const initParams = (params: Record<string, string>) => new URLSearchParams({
+  client_id: CLIENT_ID,
+  redirect_uri: redirectUri,
+  client_secret: CLIENT_SECRET,
+  ...params
+});
+
 export const getAuthTokenWithCode = async (code: string) => {
-  params.append('code', code);
-  params.append('grant_type', 'authorization_code');
+  const params = initParams({ code, grant_type: 'authorization_code' });
 
   const res = await fetch(`${authApi}/token`, {
     method: 'POST',
@@ -33,8 +33,7 @@ export const getAuthTokenWithCode = async (code: string) => {
 };
 
 export const getAuthTokenWithRefreshToken = async (refreshToken: string) => {
-  params.append('refresh_token', refreshToken);
-  params.append('grant_type', 'refresh_token');
+  const params = initParams({ refresh_token: refreshToken, grant_type: 'refresh_token' });
 
   const res = await fetch(`${authApi}/token`, {
     method: 'POST',
