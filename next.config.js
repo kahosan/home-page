cb8f724fa0103f19593159506cd808241dc07ee8
kahosan <15116355657l@gmail.com>
2023-10-06 14:49:40 +0800

    chore: remove forgetti

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const withForgetti = require('forgetti-loader/next');
-
-const nextConfig = withForgetti({
-  // Forgetti options. See https://github.com/lxsmnsyc/forgetti/tree/main#configuration for more details.
-  preset: 'react'
-})({
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   output: 'standalone'
-});
+};
 
 module.exports = nextConfig;
