535d916687b79aeb444b492f77dfce1a250a607a
kahosan <15116355657l@gmail.com>
2023-10-09 16:35:58 +0800

    feat: data type check

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -5,13 +5,28 @@ import type { Service } from 'src/types/services';
 
 const filePath = process.env.FILE_PATH || `${path.resolve()}/services.json`;
 
+const fileName = filePath.split('/').at(-1);
+
 export const getServicesData = async () => {
-  try {
-    const data = await fs.readFile(filePath, { encoding: 'utf-8' });
-    return JSON.parse(data) as Service[];
-  } catch {
-    throw new Error('获取 services data 错误');
+  const data = await fs.readFile(filePath, { encoding: 'utf-8' });
+  const servicesData = JSON.parse(data) as Service[];
+
+  if (Array.isArray(servicesData)) {
+    for (const service of servicesData) {
+      const keys = ['name', 'path', 'description', 'icon'];
+      if (!(keys.every((key) => key in service)))
+        throw new Error(`${fileName} 文件数据格式错误 ${keys.join(', ')} 为必填项`);
+
+      for (const key in service) {
+        if (typeof service[key as keyof Service] !== 'string')
+          throw new TypeError(`${fileName} 文件数据格式错误 ${key} 的值必须为字符串`);
+      }
+    }
+  } else {
+    throw new TypeError(`${fileName} 数据格式应为数组`);
   }
+
+  return servicesData;
 };
 
 export const addServicesData = async (service: Service) => {
@@ -28,10 +43,10 @@ export const deleteServicesData = async (target: string) => {
   await fs.writeFile(filePath, JSON.stringify(newData, null, 2));
 };
 
-export const editServiceData = async (service: Service & { oldName: string }) => {
+export const editServiceData = async (service: Service, id: string) => {
   const servicesData = await getServicesData();
   const newData = servicesData.map((item) => {
-    return item.name === service.oldName ? service : item;
+    return item.name === id ? service : item;
   });
 
   await fs.writeFile(filePath, JSON.stringify(newData, null, 2));
